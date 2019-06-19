22cb7cefb860e3ff2ddbd9df90f075e22b72b129
Miguel Vieira <jmvieira@gmail.com>
2019-06-19 14:58:01 +0100

    Add functions to get tags, themes, packages.

--- data_portal_explorer/cli.py
+++ data_portal_explorer/cli.py
@@ -1,18 +1,95 @@
 # -*- coding: utf-8 -*-
 
 """Console script for data_portal_explorer."""
+import json
+import os
 import sys
+
 import click
 
+import pandas as pd
+from data_portal_explorer import get_extensions, get_facets, get_packages
+from pandas.io.json import json_normalize
 
-@click.command()
-def main(args=None):
+
+@click.group(chain=True)
+@click.argument('dest', nargs=1, required=True,
+                type=click.Path(file_okay=False, resolve_path=True))
+@click.option('--format', 'fmt', default='json', show_default=True,
+              type=click.Choice(['csv', 'json']))
+@click.pass_context
+def cli(ctx, fmt, dest):
     """Console script for data_portal_explorer."""
-    click.echo("Replace this message by putting your code into "
-               "data_portal_explorer.cli.main")
-    click.echo("See click documentation at http://click.pocoo.org/")
-    return 0
+    click.echo('Data Portal Explorer')
+
+    ctx.ensure_object(dict)
+    ctx.obj['FORMAT'] = fmt
+    ctx.obj['DEST'] = dest
+
+    try:
+        os.makedirs(dest)
+    except FileExistsError:
+        pass
 
 
-if __name__ == "__main__":
-    sys.exit(main())  # pragma: no cover
+@cli.command()
+@click.pass_context
+def extensions(ctx):
+    """Gets the available extensions."""
+    click.echo('- Getting extensions')
+    data = get_extensions()
+
+    _save(ctx, 'extensions', data)
+
+
+@cli.command()
+@click.pass_context
+def tags(ctx):
+    """Gets the tags used by the datasets."""
+    click.echo('- Getting tags')
+
+    name = 'tags'
+    data = get_facets(name)
+
+    _save(ctx, name, data)
+
+
+@cli.command()
+@click.pass_context
+def themes(ctx):
+    """Gets the themes used by the datasets."""
+    click.echo('- Getting themes')
+
+    name = 'themes'
+    data = get_facets(name)
+
+    _save(ctx, name, data)
+
+
+@cli.command()
+@click.pass_context
+def packages(ctx):
+    """Gets packages."""
+    click.echo('- Getting packages')
+
+    data = get_packages()
+
+    _save(ctx, 'packages', data, normalise=True)
+
+
+def _save(ctx, filename, data, normalise=False):
+    dst_path = os.path.join(ctx.obj['DEST'], filename)
+
+    with open('{}.json'.format(dst_path), 'w') as f:
+        json.dump(data, f, indent=4, sort_keys=True)
+        f.write('\n')
+        f.close()
+
+    if ctx.obj['FORMAT'] == 'csv':
+        df = json_normalize(data) if normalise else pd.read_json(f.name)
+        df.index.name = filename
+        df.to_csv('{}.csv'.format(dst_path))
+
+
+if __name__ == '__main__':
+    sys.exit(cli())  # pragma: no cover
--- data_portal_explorer/data_portal_explorer.py
+++ data_portal_explorer/data_portal_explorer.py
@@ -1,3 +1,67 @@
 # -*- coding: utf-8 -*-
 
 """Main module."""
+
+from ckanapi import RemoteCKAN
+
+PORTALS = {
+    'data.kdl.kcl.ac.uk': {
+        'url': 'https://data.kdl.kcl.ac.uk/',
+        'themes': 'theme-primary'
+    },
+    'data.gov.uk': {
+        'url': 'https://ckan.publishing.service.gov.uk/',
+        'themes': 'theme-primary',
+        'search-page-field': 'offset',
+        'search-page-size-field': 'limit'
+    }
+}
+
+
+def get_extensions():
+    extensions = dict()
+
+    for k in PORTALS.keys():
+        portal = RemoteCKAN(PORTALS[k]['url'])
+
+        r = portal.action.status_show()
+        if r:
+            extensions[k] = {
+                name: 1 for name in r['extensions']
+            }
+
+    return extensions
+
+
+def get_facets(name):
+    facets = dict()
+
+    for k in PORTALS.keys():
+        portal = RemoteCKAN(PORTALS[k]['url'])
+
+        facet_field = PORTALS[k].get(name, name)
+
+        r = portal.call_action(
+            'package_search', {
+                'facet.field': '["{}"]'.format(facet_field), 'facet.limit': -1
+            })
+        if r:
+            data = r['facets'][facet_field]
+            facets[k] = dict(sorted(data.items(), key=lambda t: t[0].lower()))
+
+    return facets
+
+
+def get_packages():
+    packages = list()
+
+    for k in PORTALS.keys():
+        portal = RemoteCKAN(PORTALS[k]['url'])
+
+        r = portal.action.package_search()
+        if r:
+            for package in r['results']:
+                package['_portal'] = k
+                packages.append(package)
+
+    return packages
