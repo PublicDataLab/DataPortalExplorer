adf58a9ee26a5f16ef6683b0f58fdeb09b3fa882
Miguel Vieira <jmvieira@gmail.com>
2020-01-22 14:18:20 +0000

    Bump version: 0.1.1 → 0.1.2

--- data_portal_explorer/__init__.py
+++ data_portal_explorer/__init__.py
@@ -4,4 +4,4 @@
 
 __author__ = """Kings Digital Lab"""
 __email__ = 'jmvieira@gmail.com'
-__version__ = '0.1.1'
+__version__ = '0.1.2'
--- setup.py
+++ setup.py
@@ -48,6 +48,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://gitlab.com/kingsdigitallab/data_portal_explorer',
-    version='0.1.1',
+    version='0.1.2',
     zip_safe=False,
 )
