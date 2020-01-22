bf122fa0c589571cd88e5d1018c4159744322033
Miguel Vieira <jmvieira@gmail.com>
2020-01-22 14:53:20 +0000

    🐛 Fix: Porject URL.

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@
 
 from setuptools import setup, find_packages
 
-with open('README.rst') as readme_file:
+with open("README.rst") as readme_file:
     readme = readme_file.read()
 
-with open('HISTORY.rst') as history_file:
+with open("HISTORY.rst") as history_file:
     history = history_file.read()
 
-with open('requirements.txt') as requirements_file:
+with open("requirements.txt") as requirements_file:
     requirements = requirements_file.read()
 
 setup_requirements = []
@@ -20,34 +20,32 @@ test_requirements = []
 
 setup(
     author="Kings Digital Lab",
-    author_email='jmvieira@gmail.com',
+    author_email="jmvieira@gmail.com",
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Natural Language :: English',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.7',
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
     ],
     description="""This package contains tools to extract metadata from CKAN
     instances which can be used to study data portals as infrastructures from
     the perspective of social/cultural research.""",
     entry_points={
-        'console_scripts': [
-            'data_portal_explorer=data_portal_explorer.cli:cli',
-        ],
+        "console_scripts": ["data_portal_explorer=data_portal_explorer.cli:cli",],
     },
     install_requires=requirements,
     license="MIT license",
-    long_description=readme + '\n\n' + history,
+    long_description=readme + "\n\n" + history,
     include_package_data=True,
-    keywords='data_portal_explorer',
-    name='data_portal_explorer',
-    packages=find_packages(include=['data_portal_explorer']),
+    keywords="data_portal_explorer",
+    name="data_portal_explorer",
+    packages=find_packages(include=["data_portal_explorer"]),
     setup_requires=setup_requirements,
-    test_suite='tests',
+    test_suite="tests",
     tests_require=test_requirements,
-    url='https://gitlab.com/kingsdigitallab/data_portal_explorer',
-    version='0.1.3',
+    url="https://github.com/kingsdigitallab/DataPortalExplorer",
+    version="0.1.3",
     zip_safe=False,
 )
